b377ec857961ff33bfd76eb66046bea1b6191690
Julian <jb@futureplay.de>
2018-04-24 15:39:40 +0200

    added some docu for create_issue and changed the params

--- utils/issue-tracker.py
+++ utils/issue-tracker.py
@@ -10,5 +10,24 @@ for repo in g.get_user().get_repos():
         r = repo
 
 
-def create_issue(submitter, title, body):
+"""
+This function will create an issue on github once one is found.
+
+The message will look like this
+    @Bot <type of issue>; <title>; <body>
+
+So for example
+    @Bot suggestion; better money name; i don't like euro, shekles is way cooler
+    @Bot issue; comand xyz breaks on certain input; the input looks like this "blub"
+
+params:
+    client: is the client object of the bot
+    message: is the message the user typed without the ping
+
+"""
+async def create_issue(client, message):
+    submitter = ""
+    title = ""
+    body = ""
+
     r.create_issue(title=": ".join(submitter, title), body=body)
